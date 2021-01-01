c5fe7e9f63f052c9f471b796fa9e882ffdc55c12
Igor G <73964453+Pythoncraft@users.noreply.github.com>
2021-01-01 23:19:43 +0200

    Second commit
    Functional version

--- outlook_attach_downloader.py
+++ outlook_attach_downloader.py
@@ -1,16 +1,26 @@
 import win32com.client as client
-import re
 import os
 
 outlook = client.Dispatch("Outlook.Application")
 namespace = outlook.GetNamespace("MAPI")
 inbox = namespace.GetDefaultFolder(6) # "6" refers to the inbox folder. returns an object
-narbutas_folder = inbox.Folders['Noreply Narbutas'] # refering / accessing the 'Leads' subfolder
-messages = narbutas_folder.items
+index_folder = inbox.Folders['Folder name here'] # refering / accessing the needed subfolder in the inbox
+messages = index_folder.items
+folder_name = str(index_folder) # Creating a folder name
 
+try:
+	os.mkdir(os.path.join(os.getcwd(), folder_name)) # creating new directory (os.mkdir) by joining(os.path.join) current directory path(os.getcwd()) with new folder name
+except:
+	pass
+os.chdir(os.path.join(os.getcwd(), folder_name)) # change to the new directory
 for message in messages:
 	attachment = message.Attachments.Item(1) # select attachement item to save
-	attachment.SaveAsFile(os.path.join(os.getcwd(), str(attachment))) #saves filse to current directory
-	
-# Add directory creatio where to save files
+	# attachment.SaveAsFile(os.path.join(os.getcwd(), str(attachment))) # saves filse to current directory
+	attachment_1 = message.Attachments.Item(2) # select attachement item to save
+	# attachment.SaveAsFile(os.path.join(os.getcwd(), str(attachment_1))) # saves filse to current directory
+	print(attachment_1)
+	print(attachment)
+
+# Add download all attachement files in emails functional
+# Add code to check whether the file is already downloaded
 # Wrap in a function with arguments and optional filters
